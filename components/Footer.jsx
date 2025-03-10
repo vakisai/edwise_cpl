a8768b8d83a0c360c1b7a8fcc2a68d37ea5e6615
Aryan Bansal <91452049+Bansal29@users.noreply.github.com>
2025-03-11 00:37:43 +0530

    modified components

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,44 +1,104 @@
-import Link from 'next/link'
-import Image from 'next/image'
+import Link from "next/link";
+import Image from "next/image";
 
-
-const Footer =()=>{
-	return (<div className="text-white  py-10 bg-gray-800 flex items-center justify-center  w-full ">
-		<div className="footer-container container flex flex-row items-start justify-between px-10 max-sm:flex-col max-sm:items-center max-sm:h-[450px]">
-			<section className="sponser-contaier flex items-start">
-				<Image src="/logo.svg" className="mr-2" height={32} width={32} alt="logo" />
-				<section className="sponser-info flex flex-col items-start justify-between">
-					<p className="text-lg font-bold mb-1">Hostel Premier League</p>	
-					<p className="text-slate-400 text-sm mb-3">Powered By</p>
-					<Link target="_blank" href="https://www.edwiseinternational.com/">
-						<Image src="/assets/edwise.svg" height={60} width={110} className="w-auto h-auto" alt="sponser-edwise-international" />
-					</Link>
-				</section>
-			</section>
-			<section className="imp-links flex flex-col justify-between gap-2">
-				<p className="text-lg font-bold mb-4">Important Links</p>
-				<Link href="https://www.edwiseinternational.com/study-abroad-consultants/about.aspx" target="_blank" className="text-sm font-medium hover:text-slate-400 hover:underline transition-colors duration-300" >About Us</Link>
-				<Link href="/contact-us" className="text-sm font-medium hover:text-slate-400 hover:underline transition-colors duration-300" >Contact Us</Link>
-				<Link href="/terms-and-conditions" className="text-sm font-medium hover:text-slate-400 hover:underline transition-colors duration-300" >Terms & Conditions</Link>
-				<Link href="/privacy-policy" className="text-sm font-medium hover:text-slate-400 hover:underline transition-colors duration-300" >Privacy Policy</Link>
-			</section>
-			<section className="flex-col">
-				<p className="text-lg font-bold mb-4">Social Links</p>
-				<section className="flex">
-					<Link href="https://www.instagram.com/edwiseint" target="_blank" className="hover:bg-slate-600 p-1 bg-slate-700 rounded-md  transition-colors duration-100 mr-2" >
-						<Image src="/assets/instagram.svg" height={32} width={32} alt="instagram" />
-					</Link>
-					<Link href="https://www.facebook.com/edwiseinternational" target="_blank" className="hover:bg-slate-600 p-1 bg-slate-700 rounded-md transition-colors duration-100 mr-2" >
-						<Image src="/assets/facebook.svg" height={32} width={32} alt="facebook" />
-					</Link>
-					<Link href="https://www.linkedin.com/company/edwise-international/?originalSubdomain=in" target="_blank" className="hover:bg-slate-600 p-1 bg-slate-700 rounded-md transition-colors duration-100" >
-						<Image src="/assets/linkedin.svg" height={32} width={32} alt="linkedin" />
-					</Link>
-				</section>
-			</section>
-			
-		</div>
-	</div>);
-}
+const Footer = () => {
+  return (
+    <div className="text-white  py-10 bg-gray-800 flex items-center justify-center  w-full ">
+      <div className="footer-container container flex flex-row items-start justify-between px-10 max-sm:flex-col max-sm:items-center max-sm:h-[450px]">
+        <section className="sponser-contaier flex items-start">
+          <Image
+            src="/logo.svg"
+            className="mr-2"
+            height={32}
+            width={32}
+            alt="logo"
+          />
+          <section className="sponser-info flex flex-col items-start justify-between">
+            <p className="text-lg font-bold mb-1">Community Premier League</p>
+            <p className="text-slate-400 text-sm mb-3">Powered By</p>
+            <Link target="_blank" href="https://www.edwiseinternational.com/">
+              <Image
+                src="/assets/edwise.svg"
+                height={60}
+                width={110}
+                className="w-auto h-auto"
+                alt="sponser-edwise-international"
+              />
+            </Link>
+          </section>
+        </section>
+        <section className="imp-links flex flex-col justify-between gap-2">
+          <p className="text-lg font-bold mb-4">Important Links</p>
+          <Link
+            href="https://www.edwiseinternational.com/study-abroad-consultants/about.aspx"
+            target="_blank"
+            className="text-sm font-medium hover:text-slate-400 hover:underline transition-colors duration-300"
+          >
+            About Us
+          </Link>
+          <Link
+            href="/contact-us"
+            className="text-sm font-medium hover:text-slate-400 hover:underline transition-colors duration-300"
+          >
+            Contact Us
+          </Link>
+          <Link
+            href="/terms-and-conditions"
+            className="text-sm font-medium hover:text-slate-400 hover:underline transition-colors duration-300"
+          >
+            Terms & Conditions
+          </Link>
+          <Link
+            href="/privacy-policy"
+            className="text-sm font-medium hover:text-slate-400 hover:underline transition-colors duration-300"
+          >
+            Privacy Policy
+          </Link>
+        </section>
+        <section className="flex-col">
+          <p className="text-lg font-bold mb-4">Social Links</p>
+          <section className="flex">
+            <Link
+              href="https://www.instagram.com/edwiseint"
+              target="_blank"
+              className="hover:bg-slate-600 p-1 bg-slate-700 rounded-md  transition-colors duration-100 mr-2"
+            >
+              <Image
+                src="/assets/instagram.svg"
+                height={32}
+                width={32}
+                alt="instagram"
+              />
+            </Link>
+            <Link
+              href="https://www.facebook.com/edwiseinternational"
+              target="_blank"
+              className="hover:bg-slate-600 p-1 bg-slate-700 rounded-md transition-colors duration-100 mr-2"
+            >
+              <Image
+                src="/assets/facebook.svg"
+                height={32}
+                width={32}
+                alt="facebook"
+              />
+            </Link>
+            <Link
+              href="https://www.linkedin.com/company/edwise-international/?originalSubdomain=in"
+              target="_blank"
+              className="hover:bg-slate-600 p-1 bg-slate-700 rounded-md transition-colors duration-100"
+            >
+              <Image
+                src="/assets/linkedin.svg"
+                height={32}
+                width={32}
+                alt="linkedin"
+              />
+            </Link>
+          </section>
+        </section>
+      </div>
+    </div>
+  );
+};
 
 export default Footer;
--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -10,58 +10,70 @@ import { UserIcon, Bars3BottomRightIcon } from "@heroicons/react/24/outline";
 import { LoginButton, LogoutButton } from "@components/ClientComponents";
 
 const NavBar = async () => {
-	const session = await getServerSession(authOptions);
-	console.log(session?.user);
-	const is_admin=session?.user?.is_admin;
+  const session = await getServerSession(authOptions);
+  console.log(session?.user);
+  const is_admin = session?.user?.is_admin;
 
-	return (
-		<div className={`flex items-center justify-between h-14 px-4 w-full ${is_admin ? null : "bg-slate-200"}`}>
-			<Link href="/" className="font-bold flex items-center justify-between">
-				<Image height={32} width={32} alt="Logo" src="/logo.svg" />
-				<p className="text-lg min-w-fit ml-2 max-sm:hidden">Community Premier League</p>
-			</Link>
-			{
-				!is_admin && 
-			<div className="flex w-96 items-center justify-between max-md:hidden">
-				<Link href="/" className="nav-link">
-					Home
-				</Link>
-				{session?.user ? (
-					<Link href="/team-details" className="nav-link">
-						Team
-					</Link>
-				) : null}
-				<Link href="/learn-more" className="nav-link">
-					Info
-				</Link>
-				<Link href="/contact-us" className="nav-link">
-					ContactUs
-				</Link>
-				<Link href="/location" className="nav-link">
-					Location
-				</Link>
-			</div>
-			}
-			<div className="flex">
-				{session?.user ? (
-					<div className="flex gap-3 md:gap-5">
-						<LogoutButton />
-						<Link href={session.user.is_admin ? "/admin/dashboard" :"/team-details"}>
-							<Image src={session?.user.image} width={37} height={37} className="rounded-full" alt="profile" />
-						</Link>
-					</div>
-				) : (
-					<Link href="/acknowledgement">
-						<p className="btn_black rounded-full group min-w-fit">
-							<UserIcon className="h-5 w-5 text-white mr-2 group-hover:text-black transition-colors duration-75" />
-							Sign in
-						</p>
-					</Link>
-				)}
-				<Menu session={session} />
-			</div>
-		</div>
-	);
+  return (
+    <div
+      className={`flex items-center justify-between h-14 px-4 w-full ${
+        is_admin ? null : "bg-slate-200"
+      }`}
+    >
+      <Link href="/" className="font-bold flex items-center justify-between">
+        <Image height={32} width={32} alt="Logo" src="/logo.svg" />
+        <p className="text-lg min-w-fit ml-2 max-sm:hidden">
+          Community Premier League
+        </p>
+      </Link>
+      {!is_admin && (
+        <div className="flex w-96 items-center justify-between max-md:hidden">
+          <Link href="/" className="nav-link">
+            Home
+          </Link>
+          {session?.user ? (
+            <Link href="/team-details" className="nav-link">
+              Team
+            </Link>
+          ) : null}
+          <Link href="/learn-more" className="nav-link">
+            Info
+          </Link>
+          <Link href="/contact-us" className="nav-link">
+            ContactUs
+          </Link>
+        </div>
+      )}
+      <div className="flex">
+        {session?.user ? (
+          <div className="flex gap-3 md:gap-5">
+            <LogoutButton />
+            <Link
+              href={
+                session.user.is_admin ? "/admin/dashboard" : "/team-details"
+              }
+            >
+              <Image
+                src={session?.user.image}
+                width={37}
+                height={37}
+                className="rounded-full"
+                alt="profile"
+              />
+            </Link>
+          </div>
+        ) : (
+          <Link href="/acknowledgement">
+            <p className="btn_black rounded-full group min-w-fit">
+              <UserIcon className="h-5 w-5 text-white mr-2 group-hover:text-black transition-colors duration-75" />
+              Sign in
+            </p>
+          </Link>
+        )}
+        <Menu session={session} />
+      </div>
+    </div>
+  );
 };
 
 export default NavBar;
